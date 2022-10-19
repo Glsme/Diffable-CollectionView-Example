d8eefdb199668c49804e8fa4aee1675adaee6fdc
Glsme <glasses.str.man@gmail.com>
2022-10-20 01:15:03 +0900

    Feat: 화면 구성 및 IBoutlet 연결

--- ShoppingListApp/ViewController.swift
+++ ShoppingListApp/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shoppingCollectionView: UICollectionView!
+    @IBOutlet weak var shoppingSearchbar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
